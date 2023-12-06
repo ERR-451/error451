067fc9335c10cdd51ba4799fcdc43743f363ca2a
Jon Connell <con18013@byui.edu>
2023-12-06 12:36:34 -0700

    Add handling for no reviews and display average rating with decimal if necessary

--- src/StarRating.js
+++ src/StarRating.js
@@ -3,6 +3,11 @@ import React from "react";
 function StarRating(props) {
   const reviews = props.reviews;
 
+  // If there are no reviews, display a different message
+  if (reviews.length === 0) {
+    return <p>No Reviews Yet!</p>;
+  }
+
   // Calculate the average rating
   const totalStars = reviews.reduce((total, review) => {
     console.log(review); // Log each review to check its structure
@@ -10,7 +15,12 @@ function StarRating(props) {
   }, 0);
   const averageRating = reviews.length > 0 ? totalStars / reviews.length : 0;
 
-  return <p>Average Rating: {averageRating.toFixed(1)}/5</p>;
+  // Check if the average rating is a whole number
+  const displayRating = Number.isInteger(averageRating)
+    ? averageRating
+    : averageRating.toFixed(1);
+
+  return <p>Average Rating: {displayRating}/5</p>;
 }
 
 export default StarRating;
